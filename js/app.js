341f9675b071e0a257d290e1406a40975eac8796
Mike Sozanski <macsupport@gmail.com>
2017-08-25 11:29:19 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -22,7 +22,15 @@ calcChoc();
      }
 });
 
-
+var pickerDevice = myApp.picker({
+    input: '#picker-device',
+    cols: [
+        {
+            textAlign: 'center',
+            values: ['iPhone 4', 'iPhone 4S', 'iPhone 5', 'iPhone 5S', 'iPhone 6', 'iPhone 6 Plus', 'iPad 2', 'iPad Retina', 'iPad Air', 'iPad mini', 'iPad mini 2', 'iPad mini 3']
+        }
+    ]
+});
 setdrug();
   //setspin();
    //MLK();
